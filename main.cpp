cdfe521f604cf6fee7b952d8bb5cbf436604ea09
Source666 <455698977@qq.com>
2021-03-14 19:13:03 +0800

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    int arr[] = {45, 23, 63, 62, 6, 75, 76, 24, 74, 34};
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     printf("hi super fast net!\n");
     return 0;
 }
